273f996c7f74f09cf65a1effce60a6c6df1bb0ec
unknown <jlmv84Qgmail.com>
2022-04-25 22:51:11 -0400

    navvs optimizados por transicion en el medio

--- navScripts.js
+++ navScripts.js
@@ -1,14 +1,15 @@
 
-window.onscroll = function() {
+window.onscroll = () => {
     const navOne = document.getElementById('myFirstNav');
     const mySecondNav = document.getElementById('mySecondNav');
     if(document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280){
-        // navOne.classList.add('diseappea
-        mySecondNav.classList.remove('d-none');
         navOne.classList.add('d-none');
+        mySecondNav.classList.remove('custom-nav-hide');
+        mySecondNav.classList.add('custom-nav-show');
     }else {
-        mySecondNav.classList.add('d-none');
         navOne.classList.remove('d-none');
         navOne.classList.add('d-block');
+        mySecondNav.classList.add('custom-nav-hide');
+        mySecondNav.classList.remove('custom-nav-show');
     }
 };
